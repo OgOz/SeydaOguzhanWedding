d9a150fcf16ea8a56981b04286ee0b11ce3b35d1
Oguzhan Ozbektas <oguzhan.ozbektas@gmail.com>
2025-12-24 22:06:30 +0300

    chore: Secure Firebase config using environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC7Ehkjzbf-zUx8Ck48OL701qcIftn72S0",
-    authDomain: "seydaoguzhan-1c037.firebaseapp.com",
-    projectId: "seydaoguzhan-1c037",
-    storageBucket: "seydaoguzhan-1c037.firebasestorage.app",
-    messagingSenderId: "76086764224",
-    appId: "1:76086764224:web:bbd35ff453298a68eb8325",
-    measurementId: "G-354NHT5RS9"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
